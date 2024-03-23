4448ad819b6013c7e8e54c0ca54a5fe2528f4e37
Joshua Bornet <joshua.bornet@edu.devinci.fr>
2024-03-23 12:39:06 +0100

    commiting first changes (to 6.1)

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -1,14 +1,32 @@
 import bodyParser from "body-parser";
 import express from "express";
-import { BASE_ONION_ROUTER_PORT } from "../config";
+import { BASE_ONION_ROUTER_PORT, REGISTRY_PORT } from "../config";
+import { exportPrvKey, exportPubKey, generateRsaKeyPair } from "../crypto";
 
 export async function simpleOnionRouter(nodeId: number) {
   const onionRouter = express();
   onionRouter.use(express.json());
   onionRouter.use(bodyParser.json());
 
-  // TODO implement the status route
-  // onionRouter.get("/status", (req, res) => {});
+  onionRouter.get("/status", (req, res) => {
+    res.send("live");
+  });
+
+  let lastReceivedEncryptedMessage: string | null = null;
+  let lastReceivedDecryptedMessage: string | null = null;
+  let lastMessageDestination: number | null = null;
+
+  onionRouter.get("/getLastReceivedEncryptedMessage", (req, res) => {
+    res.json({ result: lastReceivedEncryptedMessage });
+  });
+
+  onionRouter.get("/getLastReceivedDecryptedMessage", (req, res) => {
+    res.json({ result: lastReceivedDecryptedMessage });
+  });
+
+  onionRouter.get("/getLastMessageDestination", (req, res) => {
+    res.json({ result: lastMessageDestination });
+  });
 
   const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
     console.log(
@@ -18,5 +36,21 @@ export async function simpleOnionRouter(nodeId: number) {
     );
   });
 
+  const keyPair = await generateRsaKeyPair();
+  const publicKey = await exportPubKey(keyPair.publicKey);
+  const privateKey = await exportPrvKey(keyPair.privateKey);
+
+  const reponse = await fetch(`http://localhost:${REGISTRY_PORT}/registerNode`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ nodeId, pubKey: publicKey }),
+  });
+
+  onionRouter.get("/getPrivateKey", async (req, res) => {
+    res.json({ result: privateKey });
+  });
+  console.log(await reponse.text());
   return server;
 }
--- src/users/user.ts
+++ src/users/user.ts
@@ -1,6 +1,7 @@
 import bodyParser from "body-parser";
 import express from "express";
-import { BASE_USER_PORT } from "../config";
+import { BASE_USER_PORT, REGISTRY_PORT } from "../config";
+import { rsaEncrypt, createRandomSymmetricKey, exportSymKey, symEncrypt } from "../crypto";
 
 export type SendMessageBody = {
   message: string;
@@ -11,14 +12,47 @@ export async function user(userId: number) {
   const _user = express();
   _user.use(express.json());
   _user.use(bodyParser.json());
+  let lastReceivedMessage: string | null = null;
+  let lastSentMessage: string | null = null;
 
-  // TODO implement the status route
-  // _user.get("/status", (req, res) => {});
+  _user.get("/status", (req, res) => {
+    res.send("live");
+  });
 
+  _user.get("/getLastReceivedMessage", (req, res) => {
+    res.json({ result: lastReceivedMessage });
+  });
+
+  _user.get("/getLastSentMessage", (req, res) => {
+    res.json({ result: lastSentMessage });
+  });
   const server = _user.listen(BASE_USER_PORT + userId, () => {
     console.log(
       `User ${userId} is listening on port ${BASE_USER_PORT + userId}`
     );
+  });
+
+  _user.post("/message", (req, res) => {
+    const { message } = req.body;
+    lastReceivedMessage = message;
+    res.send("Message received");
+  });
+
+  _user.post("/sendMessage", async (req, res) => {
+    const { message, destinationUserId } = req.body;
+    const symKey = await createRandomSymmetricKey();
+    const encryptedSymKey = await rsaEncrypt(await exportSymKey(symKey), destinationUserId);
+    const encryptedMessage = await symEncrypt(symKey, message);
+    const response = await fetch(`http://localhost:${BASE_USER_PORT + destinationUserId}/message`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: encryptedMessage }),
+    });
+    lastSentMessage = message;
+    console.log(await response.text());
+    res.send("Message sent");
   });
 
   return server;
